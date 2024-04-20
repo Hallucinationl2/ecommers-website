cb033716296224a845488a6b1f13b5c07680d20e
Radoslav <117578667+Hallucinationl2@users.noreply.github.com>
2024-04-20 21:19:45 +0300

    Add files via upload

--- payment/models.py
+++ payment/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from store.models import Product
+from django.db.models.signals import post_save
 
 
 class ShippingAddress(models.Model):
@@ -7,7 +9,7 @@ class ShippingAddress(models.Model):
     shipping_full_name = models.CharField(max_length=100)
     shipping_email = models.CharField(max_length=200)
     shipping_address1 = models.CharField(max_length=200)
-    shipping_address2 = models.CharField(max_length=200)
+    shipping_address2 = models.CharField(max_length=200, null=True, blank=True)
     shipping_city = models.CharField(max_length=50)
     shipping_state = models.CharField(max_length=50, null=True, blank=True)
     shipping_zipcode = models.CharField(max_length=20, null=True, blank=True)
@@ -18,3 +20,36 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return f'Shipping Address - {str(self.id)}'
+
+
+def create_shipping(sender, instance, created, **kwargs):
+    if created:
+        user_shipping = ShippingAddress(user=instance)
+        user_shipping.save()
+
+
+post_save.connect(create_shipping, sender=User)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
+    shipping_address = models.TextField(max_length=15000)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order - {str(self.id)}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"OrderItem - {str(self.id)}"
--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,106 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from cart.cart import Cart
+from payment.forms import ShippingForm, PaymentForm
+from payment.models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
+
+
+def process_order(request):
+    if request.POST:
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+
+        payment_form = PaymentForm(request.POST or None)
+        my_shipping = request.session.get('my_shipping')
+
+        full_name = my_shipping['shipping_full_name']
+        email = my_shipping['shipping_email']
+        shipping_address = (f"{my_shipping['shipping_address1']}\n"
+                            f"{my_shipping['shipping_address2']}\n"
+                            f"{my_shipping['shipping_city']}\n"
+                            f"{my_shipping['shipping_state']}\n"
+                            f"{my_shipping['shipping_zipcode']}\n"
+                            f"{my_shipping['shipping_country']}")
+        amount_pay = totals
+        # create order
+        if request.user.is_authenticated:
+            user = request.user
+            create_order = Order(user=user, full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_pay)
+            create_order.save()
+
+            messages.success(request, "Order placed!")
+            return redirect('home')
+        else:
+            create_order = Order(full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_pay)
+            create_order.save()
+
+            messages.success(request, "Order placed!")
+            return redirect('home')
+
+    else:
+        messages.success(request, "Access Denied")
+        return redirect('home')
+
+
+def billing_info(request):
+    if request.POST:
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+
+        my_shipping = request.POST
+        request.session['my_shipping'] = my_shipping
+
+        if request.user.is_authenticated:
+            billing_form = PaymentForm()
+
+            return render(request, 'payment/billing_info.html', {"cart_products": cart_products,
+                                                                 "quantities": quantities, "totals": totals,
+                                                                 "shipping_info": request.POST,
+                                                                 "billing_form": billing_form})
+        else:
+            billing_form = PaymentForm()
+            return render(request, 'payment/billing_info.html', {"cart_products": cart_products,
+                                                                 "quantities": quantities, "totals": totals,
+                                                                 "shipping_info": request.POST,
+                                                                 "billing_form": billing_form})
+
+        shipping_form = request.POST
+        return render(request, 'payment/billing_info.html', {"cart_products": cart_products,
+                                                             "quantities": quantities, "totals": totals,
+                                                             "shipping_form": shipping_form})
+    else:
+        messages.success(request, "Access Denied")
+        return redirect('home')
+
+
+def checkout(request):
+    cart = Cart(request)
+    cart_products = cart.get_prods()
+    quantities = cart.get_quants
+    totals = cart.cart_total()
+
+    if request.user.is_authenticated:
+        # checkout as user
+
+        shipping_user = ShippingAddress.objects.get(id=request.user.id)
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+
+        return render(request, 'payment/checkout.html', {"cart_products": cart_products,
+                                                         "quantities": quantities, "totals": totals,
+                                                         "shipping_form": shipping_form})
+    else:
+        # checkout as guest
+
+        shipping_form = ShippingForm(request.POST or None)
+
+        return render(request, 'payment/checkout.html', {"cart_products": cart_products,
+                                                         "quantities": quantities, "totals": totals,
+                                                         "shipping_form": shipping_form})
 
 
 def payment_success(request):
